865e7b7b2eb5da86bf722e5ec135d643d13a972a
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-01-20 03:44:55 +0100

    Add files via upload

---
+++ Euler2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Euler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long suma = 0;
+            int i1 = 1;
+            int i0 = 0;
+            while (i0 + i1 < 4000000)
+            {
+                if (i1 % 2 == 0)
+                {
+                    suma += i1; 
+
+                }
+                int i2 = i0;
+                i0 = i1;
+                i1 = i2 + i0;
+            }
+            suma += i1;
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+    }
+}
