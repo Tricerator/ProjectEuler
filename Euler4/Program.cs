e96e402ef04ed77fc3ace8e699f055968294d56d
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-01-20 04:17:28 +0100

    Add files via upload

---
+++ Euler4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Euler4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MAX = 0;
+            for (int i = 999; i >= 100; i--)
+            {
+                for (int j = i; j >= 100; j--)
+                {
+
+                    if ((i * j).ToString() == Reverse((i * j).ToString()))
+                    {
+                        if (i * j > MAX)
+                        {
+                            MAX = i * j;
+                            Console.WriteLine((i * j).ToString());
+                            Console.WriteLine(i.ToString() + " " + j.ToString());
+                        }
+                    }
+
+                }
+        
+            }
+
+            Console.ReadKey();
+        
+    }
+        public static string Reverse( string line)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = line.Length-1; i >= 0; i--)
+            {
+                sb.Append(line[i]);
+
+            }
+            return sb.ToString();
+        }
+    }
+}
