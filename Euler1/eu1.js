b24583ed86cac52a870eef89600d115756a13419
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-09-27 12:29:51 +0200

    Create eu1.js

---
+++ Euler1/eu1.js
@@ -0,0 +1,10 @@
+function count(){
+    range=1000
+    suma=0
+    for(i=1;i<range;i++){
+        if((i%3 === 0)||(i%5===0)){
+              suma=suma+i
+                }
+    }
+console.log(suma)
+}
