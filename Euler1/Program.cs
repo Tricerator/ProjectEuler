2b1850c25125b44e12e139ddfcac5b1328850f7d
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-09-27 12:23:10 +0200

    Add files via upload

---
+++ Euler1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int range = 1000;
+            int sum = 0;
+            for (int i = 1; i < range; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                    sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
