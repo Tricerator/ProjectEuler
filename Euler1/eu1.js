1aa4a29405cf45a76c665ab9de22ef692a3be932
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-09-30 17:11:16 +0200

    Update eu1.js

--- Euler1/eu1.js
+++ Euler1/eu1.js
@@ -1,8 +1,9 @@
+
 function count(){
     range=1000
     suma=0
-    for(i=1;i<range;i++){
-        if((i%3 === 0)||(i%5===0)){
+    for( i=1; i<range; i++){
+        if(( i%3 === 0 )||( i%5 === 0 )){
               suma=suma+i
                 }
     }
