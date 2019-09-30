e56eb227a58c78c457b30971b754db2c08fa308a
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-09-30 16:44:03 +0200

    Update Main.java

--- Euler1/Main.java
+++ Euler1/Main.java
@@ -6,13 +6,10 @@ public class Main {
 
             long result = 0;
             for(int i = 3; i < 1000; i++){
-
                 if ((i % 3 == 0) || (i % 5 == 0)){
                     result += i;
                 }
             }
             System.out.println(result);
-
         }
-
     }
