e60662fd479c86d44fe70cfd6f252110d3f4217b
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-10-02 18:33:52 +0200

    Create eu.c

---
+++ Euler1/eu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int suma = 0;
+    for (int i = 1; i < 1000; i++)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            suma += i;
+        }
+    }
+    printf("%d", suma);
+    return 0;
+}
