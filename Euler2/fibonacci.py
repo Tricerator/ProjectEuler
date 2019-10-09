358384ecf4d556ba945e514ba69078a7a8998aed
Tricerator <37005796+Tricerator@users.noreply.github.com>
2019-10-09 20:29:53 +0200

    Create fibonacci.py

---
+++ Euler2/fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(fib1,fib2,roof,mySum):
+    if fib2 > roof:
+        return mySum
+    else :
+        if fib1 % 2 == 1:
+            mySum = mySum + fib1
+        fib1 = fib1 + fib2
+        return fibonacci(fib2, fib1,roof,mySum)
+
+
+print(fibonacci(0,1,4000000,0))
