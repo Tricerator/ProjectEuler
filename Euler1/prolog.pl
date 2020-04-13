ccffb149961c72ca824e5dcbdf072f04d6df818d
Tricerator <37005796+Tricerator@users.noreply.github.com>
2020-04-14 01:15:38 +0200

    Create prolog.pl

---
+++ Euler1/prolog.pl
@@ -0,0 +1,4 @@
+solveIt(X,Z):-solveIt(1,X,0,Z).
+solveIt(X,X,A,A).
+solveIt(X,Y,A,Z):- X < Y, (0 =:= mod(X,3); 0 =:= mod(X,5)), A1 is A + X, X1 is X + 1,!, solveIt(X1,Y,A1,Z).
+solveIt(X,Y,A,Z):- X < Y, X1 is X + 1, solveIt(X1,Y,A,Z).
