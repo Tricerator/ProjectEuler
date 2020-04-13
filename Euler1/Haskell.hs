f2211f255677f287732d81339b39a3ed16bbfc70
Tricerator <37005796+Tricerator@users.noreply.github.com>
2020-04-14 00:38:34 +0200

    Update Haskell.hs
    Cool solutions

--- Euler1/Haskell.hs
+++ Euler1/Haskell.hs
@@ -1,1 +1,7 @@
  sum [ x| x <- [ 1..999 ],x `mod` 3 == 0] + sum [ x| x <- [ 1..999 ] ,x `mod` 5 == 0] - sum [ x| x <- [ 1..999 ] ,x `mod` 15 == 0]
+ 
+ --another shorter solution
+ sum [ x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+ 
+ --and another solution
+ sum [3,6..999] + sum [5,10..999] - sum [15,30..999]
