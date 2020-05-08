026da70a76fbbbc36d09650aba45b469dc7d1a2e
Tricerator <37005796+Tricerator@users.noreply.github.com>
2020-05-08 16:41:52 +0200

    Added comments

--- Euler1/prolog.pl
+++ Euler1/prolog.pl
@@ -1,3 +1,7 @@
+%solveIt(+roof,-sum):- solveIt(+currentPosition, +roof, +currentSum, -sum)
+% when currentPosition == roof, then sum = actual sum
+% so I am increasiing the sum only when currentPosition % 3 == 0 or currentPosition % 5 == 0
+
 solveIt(X,Z):-solveIt(1,X,0,Z).
 solveIt(X,X,A,A).
 solveIt(X,Y,A,Z):- X < Y, (0 =:= mod(X,3); 0 =:= mod(X,5)), A1 is A + X, X1 is X + 1,!, solveIt(X1,Y,A1,Z).
