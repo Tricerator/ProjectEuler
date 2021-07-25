2d0cd77c5aa1025a9bf6dbcff22497b448d63877
Tricerator <37005796+Tricerator@users.noreply.github.com>
2021-07-25 15:32:27 +0200

    Update Euler1.py

--- Euler1/Euler1.py
+++ Euler1/Euler1.py
@@ -4,3 +4,6 @@ for x in range(0, 1000):
     if x % 3 == 0 or x % 5 == 0:
         result: int = result + x
 print(result)
+
+## alternative
+print(sum (i for i in range(1000) if i % 3 == 0 or i % 5 == 0))
